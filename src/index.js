7299dd195817363b44e5a99cbcc359e4453d8571
Devansh3712 <devanshamity@gmail.com>
2023-06-20 10:04:13 +0530

    Create NPM package
    Signed-off-by: Devansh3712 <devanshamity@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import { getGhazalsByPoet, getGhazalsByTag } from './ghazal.js';
+import { getNazmsByPoet, getNazmsByTag } from './nazm.js';
+import {
+	getShayarisByTag,
+	getShayarisByPoet,
+	getTop20ShayarisByTag,
+	getTop20CoupletsByTag,
+	getTop20ShayarisByPoet,
+	getTop5ShayarisByDay,
+} from './shayari.js';
+import { getWordOfTheDay } from './word.js';
+
+export {
+	getGhazalsByPoet,
+	getGhazalsByTag,
+	getNazmsByPoet,
+	getNazmsByTag,
+	getShayarisByTag,
+	getShayarisByPoet,
+	getTop20ShayarisByTag,
+	getTop20CoupletsByTag,
+	getTop20ShayarisByPoet,
+	getTop5ShayarisByDay,
+	getWordOfTheDay,
+};
